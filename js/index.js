39115a0cbeef82c71a86bce4db40f346d8394e09
Daisuke <dk.joker.1107@icloud.com[credential]>
2021-09-07 15:31:33 -0700

    add mobile navbar

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
 
 const menuBtn = document.querySelector('.menu-btn');
+const navList = document.querySelector('.nav_list');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
+    navList.classList.add('open');
     menuOpen = true;
   } else {
     menuBtn.classList.remove('open');
+    navList.classList.remove('open');
     menuOpen = false;
   }
 });
+
